5051232413e7fbbce582d8c162026808497dcbb6
Sreeram Ramesh Sharma <sreeram.rameshsharma@efi.com>
2022-07-13 23:09:15 +0530

    Client and tests added closes #1

--- mssc-beer-client/src/main/java/guru/springframework/msscbeerclient/web/client/BreweryClient.java
+++ mssc-beer-client/src/main/java/guru/springframework/msscbeerclient/web/client/BreweryClient.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -30,5 +31,9 @@ public class BreweryClient {
 
     public BeerDto getBeerObject(UUID beerId){
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + beerId.toString(),BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1,beerDto);
     }
 }
