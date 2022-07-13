df83edd97d4343ba56793ee690c623fa1fb8bd41
Sreeram Ramesh Sharma <sreeram.rameshsharma@efi.com>
2022-07-13 23:10:57 +0530

    Client and tests added closes #1

--- mssc-beer-client/src/test/java/guru/springframework/msscbeerclient/web/client/BreweryClientTest.java
+++ mssc-beer-client/src/test/java/guru/springframework/msscbeerclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +21,15 @@ class BreweryClientTest {
         BeerDto beerDto = breweryClient.getBeerObject(UUID.randomUUID());
 
         assertNotNull(beerDto);
+
+    }
+
+    @Test
+    void saveNewBeer(){
+        BeerDto beerDto = BeerDto.builder().beerName("Natural Pale Ale").build();
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
 
     }
 }
